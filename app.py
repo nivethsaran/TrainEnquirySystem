1da69a2be852052bfd36be3bda0f39c104cce864
Niveth Saran <31360464+nivethsaran@users.noreply.github.com>
2020-02-28 22:32:25 +0530

    Login and Register almost done

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import *
 app = Flask(__name__)
 import sqlite3
+ROOT_FOLDER= os.path.dirname(os.path.abspath(__file__))
 
 app.config['SECRET_KEY'] = 'RailwayManagement'
 @app.route('/')
@@ -9,7 +11,7 @@ def startup():
 
 
 @app.route('/login', methods=('GET', 'POST'))
-def contact():
+def login():
       error=''
       if request.method == 'POST':
          # Form being submitted; grab data from form.
@@ -22,10 +24,34 @@ def contact():
                # Form data failed validation; try again
                error = "Please supply both first and last name"
             else:
-               # Form data is valid; move along
-               print(username,password)
-               session['username']=username
-               return redirect(url_for('another'))
+               conn = None
+               try:
+                  railway = os.path.join(ROOT_FOLDER, 'railwaydb.db')
+                  conn = sqlite3.connect(railway)
+                  # cursor=conn.execute("SHOW TABLES")
+                  # for row in cursor:
+                  # print(row)
+                  dbURL = "SELECT username,password,fname,lname,email,mobile FROM user where username=?"
+                  print(dbURL)
+                  cursor = conn.cursor()
+                  cursor.execute(dbURL,(username,))
+                  for row in cursor:
+                     if row[0]==username and row[1]==password:
+                        print("Login Successful")
+                        fullname=row[2]+' '+row[3]
+                        session['fullname']=fullname
+                        session['username']=username
+                        # session['mobile']=mobile
+                        return redirect(url_for('home'))
+                  return redirect(url_for('login'))
+               except Exception as e:
+                  print(e)
+               finally:
+                  if conn:
+                     conn.close()
+               
+
+            
          except:
             pass
 
@@ -35,12 +61,40 @@ def contact():
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
+   if request.method=='POST':
+      fname=request.form['fname']
+      lname=request.form['lname']
+      username=request.form['username']
+      password=request.form['password']
+      email=request.form['email']
+      mobile=request.form['mobile']
+      print([fname,lname,username,password,email,mobile])
+
+      conn = None
+      try:
+         railway = os.path.join(ROOT_FOLDER, 'railwaydb.db')
+         conn = sqlite3.connect(railway)
+         # cursor=conn.execute("SHOW TABLES")
+         # for row in cursor:
+            # print(row)
+         dbURL = "INSERT INTO user values(?,?,?,?,?,?)"
+         print(dbURL)
+         cursor = conn.cursor()
+         cursor.execute(dbURL,(fname,lname,username,password,email,mobile))
+         print(cursor)
+         conn.commit()
+         return redirect(url_for('login'))
+      except Exception as e:
+         print(e)
+      finally:
+         if conn:
+            conn.close()
    return render_template('signup.html')
 
 @app.route('/home')
-def another():
+def home():
    if 'username' in session:
-      return session['username']
+      return session['username']+' '+session['fullname']
    else:
       return 'yaar nee'
 
