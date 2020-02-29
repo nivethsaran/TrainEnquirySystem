1396bdcdcb3d1876f08694a6ed150221685e8eed
Niveth Saran <31360464+nivethsaran@users.noreply.github.com>
2020-02-29 22:50:07 +0530

    Almost Done

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import urllib3
 import os
 from flask import *
 app = Flask(__name__)
-import sqlite3
+import sqlite3,json
+from stationlist import *
+from tempjson import *
 ROOT_FOLDER= os.path.dirname(os.path.abspath(__file__))
 
 app.config['SECRET_KEY'] = 'RailwayManagement'
 @app.route('/')
 def startup():
-   return render_template('startup.html')
+   message=''
+   if 'username' in session:
+      message='Working'
+   return render_template('startup.html',message=message)
 
 
 @app.route('/login', methods=('GET', 'POST'))
@@ -41,6 +47,8 @@ def login():
                         fullname=row[2]+' '+row[3]
                         session['fullname']=fullname
                         session['username']=username
+                        session['mobile']=row[5]
+                        session['email']=row[4]
                         # session['mobile']=mobile
                         return redirect(url_for('home'))
                   return redirect(url_for('login'))
@@ -48,13 +56,9 @@ def login():
                   print(e)
                finally:
                   if conn:
-                     conn.close()
-               
-
-            
+                     conn.close()   
          except:
             pass
-
       # Render the sign-up page
       return render_template('login.html', message=error)
 
@@ -94,9 +98,100 @@ def signup():
 @app.route('/home')
 def home():
    if 'username' in session:
-      return session['username']+' '+session['fullname']
+      return render_template('home.html',message=session['username']+' '+session['fullname'])
    else:
-      return 'yaar nee'
+      return render_template('home.html', message='yaar nee')
+
+
+@app.route('/livetrain')
+def livetrain():
+   return render_template('livetrain.html')
+
+
+@app.route('/livestation',methods=["GET","POST"])
+def livestation():
+   # print(listrail)
+   selected=''
+   trainlist = []
+   if request.method=="POST":
+      stationcode=request.form['station'].split('-')[0]
+      # print(stationcode)
+      queryURL = 'https://api.railwayapi.com/v2/arrivals/station/'+stationcode+'/hours/8/apikey/'+apikey+'/'
+      # print(queryURL)
+      # http = urllib3.PoolManager()
+      # r = http.request('GET', queryURL)
+      data=json.loads(livestationdata);
+      # print(type(data['trains']))
+      
+      if(data['response_code']==200):
+         trainlist=data['trains']
+         # print(trainlist)
+      
+      selected=request.form['station']
+   return render_template('livestation.html',list=sorted(listrail),selected=selected,trainlist=trainlist)
+
+
+@app.route('/trainroute',methods=["GET", "POST"])
+def trainroute():
+   trainno = ''
+   trainlist = []
+   if request.method == "POST":
+      trainno = request.form['trainno']
+      # print(stationcode)
+      queryURL = 'https://api.railwayapi.com/v2/route/train/'+trainno+'/apikey/'+apikey+'/'
+      print(queryURL)
+      # http = urllib3.PoolManager()
+      # r = http.request('GET', queryURL)
+      data = json.loads(trainroutejson)
+      # print(type(data['trains']))
+
+      if(data['response_code'] == 200):
+         trainlist = data['route']
+         print(trainlist)
+   # return render_template('livestation.html', list=sorted(listrail), selected=selected, trainlist=trainlist)
+   return render_template('trainroute.html',trainlist=trainlist)
+
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+
+@app.route('/logout')
+def logout():
+   session.pop('username',None)
+   session.pop('fullname',None)
+   session.pop('mobile',None)
+   session.pop('email',None)
+   return render_template('login.html')
+
+
+
+@app.route('/addtobookmarks/<string:trainno>/<string:stationcode>')
+def addtobookmarks(trainno,stationcode):
+   print(trainno,stationcode)
+   username=session['username']
+   conn = None
+   try:
+       railway = os.path.join(ROOT_FOLDER, 'railwaydb.db')
+       conn = sqlite3.connect(railway)
+       # cursor=conn.execute("SHOW TABLES")
+       # for row in cursor:
+         # print(row)
+       dbURL = "INSERT INTO trainbookmark values(?,?,?)"
+       print(dbURL)
+       cursor = conn.cursor()
+       cursor.execute(
+           dbURL, (username, trainno, stationcode))
+       print(cursor)
+       conn.commit()
+       return redirect(url_for('livestation'))
+   except Exception as e:
+      print(e)
+   finally:
+      if conn:
+         conn.close()
+   return redirect(url_for('livestation'))
 
 if __name__=='__main__':
     app.run()
---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+    var games = ["Baseball", "Tennis", "Golf", "Cricket", "Football", "Hockey", "Badminton", "Volleyball", "Boxing", "Kabaddi", "Chess", "Long Jump", "High Jump", "Racing", "Handball", "Swimming", "Wrestling"];
+
+    $(".autocomplete").autocomplete({
+        source: games
+    });
+
+});
